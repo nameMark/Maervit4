1cf6e275781cefc747e520095c2861eb8a35d273
nameMark <marko.dufala@gmail.com>
2020-03-27 15:34:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/maervit/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.example.maervit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.maervit.MainActivity;
+import com.example.maervit.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private int SLEEP_TIMER = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_screen);
+        getSupportActionBar().hide();
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
+    }
+
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(1000 * SLEEP_TIMER);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            startActivity(intent);
+            HomeActivity.this.finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/maervit/LogoLauncher.java
@@ -0,0 +1,4 @@
+package com.example.maervit;
+
+class LogoLauncher {
+}
